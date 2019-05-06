68114b289f8b1629c5aa47e1e7d013ffc3bf0cba
Malhar Ujawane <malharujawane@gmail.com>
2019-05-06 06:01:38 -0400

    Added keypress to move paddle

--- pong.py
+++ pong.py
@@ -22,13 +22,38 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350,0)
 
-paddle_b= turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.penup()
-paddle_b.goto(0,0)
+ball= turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
 
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+wn.listen()
+wn.onkey(paddle_a_up, "w")
+wn.onkey(paddle_a_down,"s")
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down,"Down")
 
 while True:
     wn.update()
